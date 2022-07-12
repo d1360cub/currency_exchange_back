46c2214fd99d2e6c679ea7162953966cf125d149
ojosdegatonegro <diegofernandocubides@gmail.com>
2022-07-12 12:13:58 -0500

    Adds DB connection

---
+++ app.js
@@ -0,0 +1,20 @@
+const dotenv = require("dotenv");
+const express = require("express");
+
+const envFile = process.env.NODE_ENV ? `.env.${process.env.NODE_ENV}` : ".env";
+dotenv.config({ path: envFile });
+
+const configExpress = require("./config/express");
+const connectDB = require("./config/database");
+//const routes = require('./routes');
+
+const app = express();
+
+const env = process.env.NODE_ENV;
+
+if (env !== "test") {
+  connectDB();
+}
+configExpress(app);
+
+module.exports = app;
