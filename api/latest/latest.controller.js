fb735f21ce70149e1220cfd399293cceb698239c
ojosdegatonegro <diegofernandocubides@gmail.com>
2022-07-14 16:52:25 -0500

    Automatically deletes old document and adds a new one with every user request

--- api/latest/latest.controller.js
+++ api/latest/latest.controller.js
@@ -1,8 +1,14 @@
-const { createLatest, getLatest } = require("./latest.service");
+const { createLatest, getLatest, deleteLatest } = require("./latest.service");
+const fetch = require("node-fetch");
+const API_URL = "https://openexchangerates.org/api/";
 
 async function handlerCreateLatest(req, res) {
+  const getData = await fetch(
+    `${API_URL}latest.json?app_id=${process.env.APP_ID}`
+  );
+  const data = await getData.json();
   try {
-    const latest = await createLatest();
+    const latest = await createLatest(data);
     res.status(201).json(latest);
   } catch (error) {
     res.status(500).json(error);
@@ -10,7 +16,13 @@ async function handlerCreateLatest(req, res) {
 }
 
 async function handlerGetLatest(req, res) {
+  deleteLatest();
+  const getData = await fetch(
+    `${API_URL}latest.json?app_id=${process.env.APP_ID}`
+  );
+  const data = await getData.json();
   try {
+    await createLatest(data);
     const latest = await getLatest();
     res.status(200).json(latest);
   } catch (error) {
--- api/latest/latest.service.js
+++ api/latest/latest.service.js
@@ -1,19 +1,7 @@
-const fetch = require("node-fetch");
 const LatestModel = require("./latest.model");
-const API_URL = "https://openexchangerates.org/api/";
 
-async function createLatest() {
-  try {
-    const response = await fetch(
-      `${API_URL}latest.json?app_id=${process.env.APP_ID}`
-    );
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    console.log(error);
-  }
-  const test = { base: "usd" };
-  const latest = await LatestModel.create();
+async function createLatest(last) {
+  const latest = await LatestModel.create(last);
   return latest;
 }
 
@@ -22,4 +10,8 @@ async function getLatest() {
   return latest;
 }
 
-module.exports = { createLatest, getLatest };
+async function deleteLatest() {
+  const deleted = await LatestModel.deleteMany();
+}
+
+module.exports = { createLatest, getLatest, deleteLatest };
